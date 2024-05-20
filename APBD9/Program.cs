062e8e2256708a91d5a6ffdb3a1ce1a0fbef57e5
s27185 <s27185@pjwstk.edu.pl>
2024-05-20 09:41:35 +0200

    Commit

---
+++ APBD9/Program.cs
@@ -0,0 +1,30 @@
+public class Program
+{
+    
+    public static void Main(string[] args)
+    {
+        
+        var builder = WebApplication.CreateBuilder(args);
+        
+        //Registering services
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+        // builder.Services.AddScoped<IAnimalService, AnimalService>();
+        // builder.Services.AddSingleton<IAnimalRepository,AnimalRepository>();
+
+        var app = builder.Build();
+
+        //Configuring the HTTP request pipeline
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.MapControllers();
+
+        app.Run();
+    }
+}
